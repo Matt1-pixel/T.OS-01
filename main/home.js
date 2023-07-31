f084b55842dde6ff6fc6761a839a97c9bd14f6cb
matheus <matheusmat2016@gmail.com>
2023-07-30 22:47:30 -0300

    deploy

--- main/home.js
+++ main/home.js
@@ -1,8 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const  = document.querySelector("");
 
-    function ...() {
-        
-    };
+    const screenElement = document.querySelector(".screen");
+    const entradaValue = document.querySelector(".entrada");
+
+    entradaValue.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            let entradaElement = entradaValue.value;
+
+            if (entradaElement.toLowerCase() === "help") {
+                screenElement.textContent = "Para anotar digite: NotePad \nPara calcular digite: Calculator \n"
+            } 
+            else {
+                // Se a entrada não for "help", limpar a tela
+                screenElement.textContent = "Digite 'Help' para ver os comandos necessarios.";
+            }
+
+            // Limpar o campo de entrada após pressionar Enter
+            //entradaValue.value = "";
+        }
+    });
 
 });
