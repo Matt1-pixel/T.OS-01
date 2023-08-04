d44504a2ff90c490e532bf9fb16d6370ffa1036a
matheus <matheusmat2016@gmail.com>
2023-08-03 23:51:01 -0300

    sss

--- main/home.js
+++ main/home.js
@@ -8,15 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
             let entradaElement = entradaValue.value;
 
             if (entradaElement.toLowerCase() === "help") {
-                screenElement.textContent = "Para anotar digite: NotePad \nPara calcular digite: Calculator \n"
+                screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: Play.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\nPara acessar o chatbot digite: chatbot.\n\n"
             } 
             else {
-                // Se a entrada não for "help", limpar a tela
-                screenElement.textContent = "Digite 'Help' para ver os comandos necessarios.";
+                // Se a entrada não for "help"
+                screenElement.textContent = "Digite 'Help' para ver os comandos validos.";
             }
 
             // Limpar o campo de entrada após pressionar Enter
-            //entradaValue.value = "";
+            entradaValue.value = "";
         }
     });
 
