b5c40b84179951295b37ea7087e6895687bbb5bd
matheus <matheusmat2016@gmail.com>
2023-08-06 02:23:34 -0300

    madruga

--- main/home.js
+++ main/home.js
@@ -8,9 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
             let entradaElement = entradaValue.value;
 
             if (entradaElement.toLowerCase() === "help") {
-                screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: Play.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\nPara acessar o chatbot digite: chatbot.\n\n"
-            } 
-            else {
+                screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: Play.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\n>Para acessar o chatbot digite: chatbot.\n\n"
+
+            }else if(entradaElement.toLowerCase() === "notepad") {
+                window.location.href = "NotePad/notePad.html"
+            }else {
                 // Se a entrada não for "help"
                 screenElement.textContent = "Digite 'Help' para ver os comandos validos.";
             }
