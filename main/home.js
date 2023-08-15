73301d2eb533ca6c2a47a009d34a8bcaa4a87406
matheus <matheusmat2016@gmail.com>
2023-08-14 23:01:07 -0300

    calc

--- main/home.js
+++ main/home.js
@@ -9,12 +9,17 @@ document.addEventListener("DOMContentLoaded", function () {
             let entradaElement = entradaValue.value;
 
             if (entradaElement.toLowerCase() === "help") {
-                screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: Play.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\n>Para acessar o chatbot digite: chatbot.\n\n"
+                screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: Play.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\n>Para ver a previsão do tempo digite: >Para acessar o chatbot digite: chatbot.\n\n"
+
 
             }else if(entradaElement.toLowerCase() === "notepad") {
                 window.location.href = "NotePad/notePad.html"
                 
-            }else {
+
+            } else if (entradaElement.toLowerCase() === "calculator") {
+                window.location.href = "Calculator/calc.html"
+
+            } else {
                 // Se a entrada não for valor valido
                 screenElement.textContent = "Digite 'Help' para ver os comandos validos.";
             }
