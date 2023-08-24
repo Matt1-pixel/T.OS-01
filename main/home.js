e2a87d473987af5393d593deb6bd5e5354338c2d
matheus <matheusmat2016@gmail.com>
2023-08-23 23:21:14 -0300

    goooo

--- main/home.js
+++ main/home.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
             switch (entradaElement) {
                 case "help":
-                    screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: Play.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\n>Para ver a previsão do tempo digite: weather.\n\n>Para acessar o chatbot digite: chatbot.\n\n";
+                    screenElement.textContent = ">Para anotar digite: NotePad.\n\n>Para calcular digite: Calculator.\n\n>Para jogar digite: game.\n\n>Para escutar musica digite: music.\n\n>Para acessar a biblioteca digite: library.\n\n>Para ver a previsão do tempo digite: weather.\n\n>Para acessar o chatbot digite: chatbot.\n\n";
                     break;
                 case "notepad":
                     window.location.href = "NotePad/notePad.html";
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 case "music":
                     window.location.href = "music player/music.html";
                     break;
-                case "weather":
-                    
+                case "game":
+                    window.location.href = "game/game.html";
                     break;
                 case "chatbot":
                     break;
